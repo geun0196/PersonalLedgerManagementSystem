818078d29a0a12ab8cb69ee8f854aa99fee97a0f
geun0196 <geun0196@naver.com>
2021-03-09 17:19:50 +0900

    removeed hello.java and added MenuManager.java

---
+++ src/MenuManager.java
@@ -0,0 +1,13 @@
+
+public class MenuManager {
+
+	public static void main(String[] args) {
+		System.out.println("1.Add Ledger");
+		System.out.println("2.Delete Ledger");
+		System.out.println("3.Edit Ledger");
+		System.out.println("4.View Ledger");
+		System.out.println("5.Exit");
+
+	}
+
+}
